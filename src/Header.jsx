79d97d4086ac5ee756721e1680c6664eb51e10fb
raako71 <mail@raako.net>
2024-01-02 17:06:00 -0300

    header JS float

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,49 @@
+import { useState } from "react";
 
-function Header() {
+const DropdownMenu = () => {
+    return (
+      <div className="dropdown-menu">
+        <ul>
+          <li>Logout</li>
+          <li>Menu 2</li>
+          <li>Menu 3</li>
+        </ul>
+      </div>
+    );
+  };
+
+
+
+function Header({ isLoggedIn }) {
+
+    const [isDropdownVisible, setDropdownVisible] = useState(true);
+
+  const handleMouseEnter = () => {
+    setDropdownVisible(true);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdownVisible(false);
+  };
+
+
+
     return (
         <header>
             <h1>hypa.space</h1>
                 <nav className="navbar">
                 <ul>
                     <li><a href="./">Home</a></li>
-                    <li><a href="./login">Login</a></li>
+                    {isLoggedIn ? (
+                        <>
+                        <li><a href="./account" onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}>Account</a>
+                        {isDropdownVisible && <DropdownMenu />}
+                        </li>
+                        </>
+                    ) : (
+                        <li><a href="./login">Login</a></li>
+                    )}
                 </ul>
                 </nav>
             
