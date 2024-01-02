156231bf619aef03e632d8457e0addb3a0a60a01
raako71 <mail@raako.net>
2024-01-02 17:31:17 -0300

    using UL for Nav

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 
 const DropdownMenu = () => {
     return (
-      <div className="dropdown-menu">
-        <ul>
-          <li>Logout</li>
-          <li>Menu 2</li>
-          <li>Menu 3</li>
-        </ul>
-      </div>
+        <div className="dropdown-menu">
+            <ul>
+                <li>Logout</li>
+                <li>Menu 2</li>
+                <li>Menu 3</li>
+            </ul>
+        </div>
     );
-  };
+};
 
 
 
@@ -18,35 +18,34 @@ function Header({ isLoggedIn }) {
 
     const [isDropdownVisible, setDropdownVisible] = useState(true);
 
-  const handleMouseEnter = () => {
-    setDropdownVisible(true);
-  };
+    const handleMouseEnter = () => {
+        setDropdownVisible(true);
+    };
 
-  const handleMouseLeave = () => {
-    setDropdownVisible(false);
-  };
+    const handleMouseLeave = () => {
+        setDropdownVisible(false);
+    };
 
 
 
     return (
         <header>
             <h1>hypa.space</h1>
-                <nav className="navbar">
+            <nav className="navbar">
                 <ul>
                     <li><a href="./">Home</a></li>
                     {isLoggedIn ? (
-                        <>
-                        <li><a href="./account" onMouseEnter={handleMouseEnter}
-                        onMouseLeave={handleMouseLeave}>Account</a>
-                        {isDropdownVisible && <DropdownMenu />}
-                        </li>
-                        </>
+                            <li><a href="./account">Account</a>
+                                <ul className="submenu">
+                                    <li><a href="">Logout</a></li>
+                                </ul>
+                            </li>
                     ) : (
                         <li><a href="./login">Login</a></li>
                     )}
                 </ul>
-                </nav>
-            
+            </nav>
+
         </header>
     );
 }
