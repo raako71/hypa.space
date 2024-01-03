9dd4a056225ec472769ccb56a4c54f3ee8251e49
raako71 <mail@raako.net>
2024-01-03 00:29:09 -0300

    New Account creation working

--- src/pages/account.jsx
+++ src/pages/account.jsx
@@ -1,8 +1,32 @@
+import React, { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
 export default function Account(){
+    const [userEmail, setUserEmail] = useState(null);
+
+    useEffect(() => {
+        const auth = getAuth();
+        
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+          if (user) {
+            // User is signed in, get the email
+            setUserEmail(user.email);
+          } else {
+            // No user signed in
+            setUserEmail(null);
+          }
+        });
+    
+        return () => {
+          unsubscribe(); // Cleanup the listener on component unmount
+        };
+      }, []);
+
     return (
     <>
     <div className="article">
     <h1>account</h1>
+    <p>email: {userEmail}</p>
     </div>
     </>
     )
